6f26a369f5a2f503f689bf13ccb3dca34697a4ed
naveengv7 <98863524+naveengv7@users.noreply.github.com>
2022-04-05 14:34:49 -0400

    Update urls.py

--- demo_django/myapp/urls.py
+++ demo_django/myapp/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+
+    path('',views.home,name='home'),
+    path('admin/', views.admin, name='admin'),
+    path('admin/f1/', views.f1),
+    path('admin/f2/', views.f2),
+    path('admin/f3/', views.f3),
+    path('professor/', views.professor, name='professor'),
+    path('professor/courses/', views.professorCourses),
+    path('professor/students/', views.professorStudents),
+    path('student/', views.student, name='student'),
+    path('student/studentCourses/', views.studentCourses),
 
 ]
-
